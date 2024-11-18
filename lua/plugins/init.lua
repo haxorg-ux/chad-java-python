4ee1d63633f7f6ed2789d735268547b7dfb46937
m4teo <josheechevarrieta@gmail.com>
2024-11-18 20:01:20 -0300

    add nvim dap go

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,6 +19,8 @@ return {
         "vim",
         "lua",
         "vimdoc",
+        "markdown",
+        "markdown_inline",
         "html",
         "css",
         "python",
@@ -76,6 +78,34 @@ return {
     },
     config = function()
       require "configs.dap-java"
+    end,
+  },
+  {
+    "leoluz/nvim-dap-go",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      require("dap-go").setup()
+      local dap, dapui = require "dap", require "dapui"
+
+      require("dapui").setup()
+      require("dap-go").setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
     end,
   },
 
